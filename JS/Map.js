75dacaa7e25135530e1dbde0b545912c421116d0
AnxianZhang <gadanxianzhang@gmail.com>
2022-12-05 17:27:17 +0100

    popup added

--- JS/Map.js
+++ JS/Map.js
@@ -1,26 +1,4 @@
-const lesCentres = [ // in order
-    [48.863375896070295, 2.3361572005665012],
-    [48.86886239211341, 2.3430180682787083],
-    [48.86331664341219, 2.3607139147670617],
-    [48.85501703126173, 2.3579802573781885],
-    [48.84490629727054, 2.350728590055977],
-    [48.85050520773673, 2.333989205095406],
-    [48.856353173406255, 2.312621149708161],
-    [48.873309248371974, 2.3122331727270713],
-    [48.87780047280575, 2.3384485463100275],
-    [48.87702010936517, 2.3609094022830326],
-    [48.85747839833945, 2.379765137220683],
-    [48.83686727528985, 2.41614682874437],
-    [48.82938688720201, 2.363206557150762],
-    [48.83169592045265, 2.3276497843958124],
-    [48.8408681862523, 2.2923728253480444],
-    [48.86033989255914, 2.2550441671290984],
-    [48.888160939619176, 2.3084997469975317],
-    [48.89342613034521, 2.349256323305188],
-    [48.88785350693627, 2.386651248151317],
-    [48.86309270659322, 2.402023206892703]
-];
-
+/*
 const getInformationFromAPI = () => {
     var url = "https://serpapi.com/search.json";
     var data = {};
@@ -41,20 +19,25 @@ const getInformationFromAPI = () => {
             let v2 = retour[0].longitude;
             monMarquer = L.marker(v, v2, mesOptions);
         },
-        error: function() {
+        error: function () {
             alert("PB avec l'URL");
         }
-    });   
+    });
 }
+*/
 
 const startMap = () => {
     mapProps = {
         center: [48.8418565, 2.2683737],
         zoom: 14
     };
+
     let map = L.map('map', mapProps);
     let base = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
+    /**
+     * les icons serons tous charger ici 
+     */
     let myIcon = L.icon({
         iconUrl: 'icon.png',
         iconSize: [25, 25],
@@ -69,10 +52,25 @@ const startMap = () => {
         alt: "sup"
     };
 
-    let myMarker = L.marker([48.8418565, 2.2683737], myOptions);
-    myMarker.addTo(map).bindPopup("Ici c'est l'iut de Paris :))");
+    /**
+     * les popup des icons:
+     */
 
-    map.on('click', onMapClick);
+    let HTMLPopUp = "<h2>IUT de paris</h2><center><img width='50px' height= '50px' src='../icon.png' alt='iut img' />Bonjour</center>";
+    let popup = L.popup({
+        keepInView: true,
+        closeButton: true
+    });
+    popup.setContent(HTMLPopUp);
+
+    let myMarker = L.marker([48.8418565, 2.2683737], myOptions);
+    myMarker.addTo(map).bindPopup(popup);
+
+
+    // ajout des évenements sur les icons
+
+    //myIcon.on("click", myIcon.openPopup(popup));
+    //map.on('click', onMapClick);
 }
 
 const init = () => {
