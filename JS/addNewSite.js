98b3c90214bd6745c3a017dc960ff9dc91a67f1d
AnxianZhang <gadanxianzhang@gmail.com>
2022-12-24 13:17:38 +0100

    new site form added

---
+++ JS/addNewSite.js
@@ -0,0 +1,14 @@
+// const initAddNewSite = () =>{
+//     Array.from(document.querySelectorAll(".nav_menu")).forEach(element =>{
+//         element.addEventListener("click", ()=>{
+//             element.classList.add("open");
+//             Array.from(document.querySelectorAll(".nav_menu")).forEach(elem =>{
+//                 if (elem != element){
+//                     elem.classList.remove("open");
+//                 }
+//             });
+//         });
+//     });
+// }
+
+// window.addEventListener("DOMContentLoaded", initAddNewSite);
