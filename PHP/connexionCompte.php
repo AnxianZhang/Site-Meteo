8a4efdc4538ce8fa72620b761afefb2a787a3ea2
linxingt <96918000+linxingt@users.noreply.github.com>
2022-12-24 18:52:56 +0100

    Add files via upload

---
+++ PHP/connexionCompte.php
@@ -0,0 +1,39 @@
+<?php
+    $username = isset($_POST['userName']) ? $_POST['userName'] : "nothing in userName";
+    $password = isset($_POST['password']) ? $_POST['password'] : "nothing in password";
+
+    function isExistingAcount($username){
+        require("connexionPDO.php");
+        $sql = "SELECT mdp
+                FROM USER_DATA
+                WHERE mail=:username;";
+        try{
+            $commande = $pdo->prepare($sql);
+            $commande->bindParam(':username', $username);
+            // complet
+            if ($commande->execute()){
+                $result = $commande->fetchAll(PDO::FETCH_ASSOC);
+                if (count($result) == 0) return "";
+                
+                return $result[0]["mdp"];
+            }
+        } catch(PDOException $e){
+            echo utf8_encode("Echec de la requete SQL dans creationCompte" . $e->getMessage() . "\n");
+            die();
+        }
+    }
+
+    $bdMdp=isExistingAcount($username);
+    if($bdMdp!=""){ // compte  existe
+        if(password_verify($password, $bdMdp)){
+            echo "connexion reussi";
+        }
+        else{
+            echo "mdp faux";
+        }
+    }
+    else{
+        echo "compte n'existe pas";
+    }
+    
+?>
